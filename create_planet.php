01848fd39e49fa7be881315f3457bfd898c68281
Nicholas Fletcher <100875284+LegendOfGrudge@users.noreply.github.com>
2022-06-19 18:58:52 -0500

    Updated to include image upload.
    Added image upload and removal when creating or updating planets. Still working on resizing.

--- create_planet.php
+++ create_planet.php
@@ -7,7 +7,46 @@
 		exit;
 	}
 	
-	if($_POST && !empty($_POST['name']) && !empty($_POST['diameter']) && !empty($_POST['climate']) && !empty($_POST['terrain']) && !empty($_POST['surface_water']) && !empty($_POST['population']))
+	// file_upload_path() - Safely build a path String that uses slashes appropriate for our OS.
+    // Default upload path is an 'uploads' sub-folder in the current folder.
+    function file_upload_path($original_filename, $upload_subfolder_name = 'uploads') {
+       $current_folder = dirname(__FILE__);
+       
+       // Build an array of paths segment names to be joins using OS specific slashes.
+       $path_segments = [$current_folder, $upload_subfolder_name, basename($original_filename)];
+       
+       // The DIRECTORY_SEPARATOR constant is OS specific.
+       return join(DIRECTORY_SEPARATOR, $path_segments);
+    }
+
+    // file_is_an_image() - Checks the mime-type & extension of the uploaded file for "image-ness".
+    function file_is_an_image($temporary_path, $new_path) {
+        $allowed_mime_types      = ['image/gif', 'image/jpeg', 'image/png'];
+        $allowed_file_extensions = ['gif', 'jpg', 'jpeg', 'png'];
+        
+        $actual_file_extension   = pathinfo($new_path, PATHINFO_EXTENSION);
+        $actual_mime_type        = getimagesize($temporary_path)['mime'];
+        
+        $file_extension_is_valid = in_array($actual_file_extension, $allowed_file_extensions);
+        $mime_type_is_valid      = in_array($actual_mime_type, $allowed_mime_types);
+        
+        return $file_extension_is_valid && $mime_type_is_valid;
+    }
+    
+    $image_upload_detected = isset($_FILES['image']) && ($_FILES['image']['error'] === 0);
+    $upload_error_detected = isset($_FILES['image']) && ($_FILES['image']['error'] > 0);
+
+    if ($image_upload_detected) { 
+        $image_filename        = $_FILES['image']['name'];
+        $temporary_image_path  = $_FILES['image']['tmp_name'];
+        $new_image_path        = file_upload_path($image_filename);
+        if (file_is_an_image($temporary_image_path, $new_image_path)) {
+			move_uploaded_file($temporary_image_path, $new_image_path);
+			$file_path = 'uploads/' . $image_filename;
+        }
+    }
+	
+	if($_POST && !empty($_POST['name']) && !empty($_POST['diameter']) && !empty($_POST['climate']) && !empty($_POST['terrain']) && !empty($_POST['surface_water']) && !empty($_POST['population']) && !isset($file_path))
 	{
 		$name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 		$diameter = filter_input(INPUT_POST, 'diameter', FILTER_SANITIZE_NUMBER_INT);
@@ -32,6 +71,32 @@
 			exit;
 		}
 	}
+	else if($_POST && !empty($_POST['name']) && !empty($_POST['diameter']) && !empty($_POST['climate']) && !empty($_POST['terrain']) && !empty($_POST['surface_water']) && !empty($_POST['population']) && isset($file_path))
+	{
+		$name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+		$diameter = filter_input(INPUT_POST, 'diameter', FILTER_SANITIZE_NUMBER_INT);
+		$climate = filter_input(INPUT_POST, 'climate', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+		$terrain = filter_input(INPUT_POST, 'terrain', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+		$surface_water = filter_input(INPUT_POST, 'surface_water', FILTER_SANITIZE_NUMBER_INT);
+		$population = filter_input(INPUT_POST, 'population', FILTER_SANITIZE_NUMBER_INT);
+		
+		$query = "INSERT INTO planets (name, diameter, climate, terrain, surface_water, population, image) VALUES (:name, :diameter, :climate, :terrain, :surface_water, :population, :image)";
+		$statement = $db->prepare($query);
+		
+		$statement->bindValue(':name', $name);
+		$statement->bindValue(':diameter', $diameter);
+		$statement->bindValue(':climate', $climate);
+		$statement->bindValue(':terrain', $terrain);
+		$statement->bindValue(':surface_water', $surface_water);
+		$statement->bindValue(':population', $population);
+		$statement->bindValue(':image', $file_path);
+		
+		if($statement->execute())
+		{
+			header("Location: planet.php?name={$name}");
+			exit;
+		}
+	}
 ?>
 <!DOCTYPE html>
 <html lang = "en">
@@ -43,7 +108,10 @@
 <body>
 <?php include("header.php") ?>	
 	<section>
-		<form id="update_form" method="post" action="create_planet.php">
+		<form id="update_form" method="post" enctype='multipart/form-data' action="create_planet.php">
+			<label for='image'>Image Filename:</label>
+			<input type='file' name='image' id='image'>
+			<br />
 			<label for="name">Name:</label>
 			<input type="text" id="name" name="name" value="" />
 			<br />
--- update_planet.php
+++ update_planet.php
@@ -7,7 +7,62 @@
 		exit;
 	}
 	
-	if($_POST && isset($_POST['diameter']) && isset($_POST['climate']) && isset($_POST['terrain']) && isset($_POST['surface_water']) && isset($_POST['population']))
+	// file_upload_path() - Safely build a path String that uses slashes appropriate for our OS.
+    // Default upload path is an 'uploads' sub-folder in the current folder.
+    function file_upload_path($original_filename, $upload_subfolder_name = 'uploads') {
+       $current_folder = dirname(__FILE__);
+       
+       // Build an array of paths segment names to be joins using OS specific slashes.
+       $path_segments = [$current_folder, $upload_subfolder_name, basename($original_filename)];
+       
+       // The DIRECTORY_SEPARATOR constant is OS specific.
+       return join(DIRECTORY_SEPARATOR, $path_segments);
+    }
+
+    // file_is_an_image() - Checks the mime-type & extension of the uploaded file for "image-ness".
+    function file_is_an_image($temporary_path, $new_path) {
+        $allowed_mime_types      = ['image/gif', 'image/jpeg', 'image/png'];
+        $allowed_file_extensions = ['gif', 'jpg', 'jpeg', 'png'];
+        
+        $actual_file_extension   = pathinfo($new_path, PATHINFO_EXTENSION);
+        $actual_mime_type        = getimagesize($temporary_path)['mime'];
+        
+        $file_extension_is_valid = in_array($actual_file_extension, $allowed_file_extensions);
+        $mime_type_is_valid      = in_array($actual_mime_type, $allowed_mime_types);
+        
+        return $file_extension_is_valid && $mime_type_is_valid;
+    }
+    
+    $image_upload_detected = isset($_FILES['image']) && ($_FILES['image']['error'] === 0);
+    $upload_error_detected = isset($_FILES['image']) && ($_FILES['image']['error'] > 0);
+
+    if ($image_upload_detected) { 
+        $image_filename        = $_FILES['image']['name'];
+        $temporary_image_path  = $_FILES['image']['tmp_name'];
+        $new_image_path        = file_upload_path($image_filename);
+        if (file_is_an_image($temporary_image_path, $new_image_path)) {
+			move_uploaded_file($temporary_image_path, $new_image_path);
+			$file_path = 'uploads/' . $image_filename;
+        }
+    }
+	
+	if($_POST && isset($_POST['remove']))
+	{
+		$planet_id = filter_input(INPUT_POST, 'planet_id', FILTER_SANITIZE_NUMBER_INT);
+		$name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+		
+		$query = "UPDATE planets SET image ='' WHERE planet_id = :planet_id";
+		$statement = $db->prepare($query);
+		$statement->bindValue(':planet_id', $planet_id, PDO::PARAM_INT);
+		
+		if($statement->execute())
+		{
+			header("Location: planet.php?name={$name}");
+			exit;
+		}
+	}
+	
+	if($_POST && isset($_POST['diameter']) && isset($_POST['climate']) && isset($_POST['terrain']) && isset($_POST['surface_water']) && isset($_POST['population']) && !isset($file_path))
 	{
 		$planet_id = filter_input(INPUT_POST, 'planet_id', FILTER_SANITIZE_NUMBER_INT);
 		$name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
@@ -24,6 +79,32 @@
 		$statement->bindValue(':terrain', $terrain);
 		$statement->bindValue(':surface_water', $surface_water);
 		$statement->bindValue(':population', $population);
+		$statement->bindValue(':planet_id', $planet_id, PDO::PARAM_INT);
+		
+		if($statement->execute())
+		{
+			header("Location: planet.php?name={$name}");
+			exit;
+		}
+	}
+	else if($_POST && isset($_POST['diameter']) && isset($_POST['climate']) && isset($_POST['terrain']) && isset($_POST['surface_water']) && isset($_POST['population']) && isset($file_path))
+	{
+		$planet_id = filter_input(INPUT_POST, 'planet_id', FILTER_SANITIZE_NUMBER_INT);
+		$name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+		$diameter = filter_input(INPUT_POST, 'diameter', FILTER_SANITIZE_NUMBER_INT);
+		$climate = filter_input(INPUT_POST, 'climate', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+		$terrain = filter_input(INPUT_POST, 'terrain', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+		$surface_water = filter_input(INPUT_POST, 'surface_water', FILTER_SANITIZE_NUMBER_INT);
+		$population = filter_input(INPUT_POST, 'population', FILTER_SANITIZE_NUMBER_INT);
+		
+		$query = "UPDATE planets SET diameter = :diameter, climate = :climate, terrain = :terrain, surface_water = :surface_water, population = :population, image = :image WHERE planet_id = :planet_id";
+		$statement = $db->prepare($query);
+		$statement->bindValue(':diameter', $diameter);
+		$statement->bindValue(':climate', $climate);
+		$statement->bindValue(':terrain', $terrain);
+		$statement->bindValue(':surface_water', $surface_water);
+		$statement->bindValue(':population', $population);
+		$statement->bindValue(':image', $file_path);
 		$statement->bindValue(':planet_id', $planet_id, PDO::PARAM_INT);
 		
 		if($statement->execute())
@@ -79,11 +160,18 @@
 	<section>
 		<?php if($planet_id): ?>
 			<h1><?= $planet['name'] ?></h1>
-		
-			<form id="update_form" method="post" action="update_planet.php">
+			
+			<form id="update_form" method="post" enctype='multipart/form-data' action="update_planet.php">
 				<input type="hidden" name="planet_id" value="<?= $planet['planet_id'] ?>">
 				<input type="hidden" name="name" value="<?= $planet['name'] ?>">
 				
+				<?php if(empty($planet['image'])): ?>
+					<label for='image'>Image Filename:</label>
+					<input type='file' name='image' id='image'>
+					<br />
+				<?php else: ?>
+					<input type="submit" name="remove" value="Remove Image" />
+				<?php endif ?>
 				<label for="diameter">Diameter:</label>
 				<input type="text" id="diameter" name="diameter" value="<?= $planet['diameter'] ?>" />
 				<br />
